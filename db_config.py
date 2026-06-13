b064b0d1b36ddf002b29ac9d99a75287f2af5e27
PrashantAtigre <prashantatigre255@gmail.com>
2026-06-13 11:00:47 +0530

    Initial Commit

--- db_config.py
+++ db_config.py
@@ -4,8 +4,8 @@ def get_database_connection():
 
     connection = mysql.connector.connect(
         host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
-        user="3ECPv9bRuy8XxSa.root",
-        password="QaZogTGI1k1ATUlI",
+        user="2MVGo7Mw7RniPYU.root",
+        password="TwbpiQ3a5HWBZB7z",
         database="student_task_manager",
         port=4000
     )
